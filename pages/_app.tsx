42879f49f00b3be667207b249936bf36672ea573
Fozle Rabbi Shafi <frshafi49@gmail.com>
2022-07-05 03:42:40 +0600

    added SWR and NAV

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
-import * as React from 'react';
-import Head from 'next/head';
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
+import * as React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
+
+import { SWRConfig } from "swr";
+import axios from "axios";
+import { Nav } from "../src/components/Nav";
+axios.defaults.baseURL = "http://localhost:4001";
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -24,7 +29,12 @@ export default function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <Nav />
+        <SWRConfig
+          value={{ fetcher: (url: string) => axios(url).then((r) => r.data) }}
+        >
+          <Component {...pageProps} />
+        </SWRConfig>
       </ThemeProvider>
     </CacheProvider>
   );
---
+++ src/components/Nav.tsx
@@ -0,0 +1,36 @@
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import Link from 'next/link';
+import React from 'react';
+
+export function Nav() {
+
+  return (
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <Typography variant="h6" >
+          Car Trader
+        </Typography>
+
+        <Button color="inherit">
+          <Link href="/">
+            <a style={{ color: 'white' }}>
+              <Typography  color="inherit">
+                Home
+              </Typography>
+            </a>
+          </Link>
+        </Button>
+
+        <Button color="inherit">
+          <Link href="/faq">
+            <a style={{ color: 'white' }}>
+              <Typography  color="inherit">
+                FAQ
+              </Typography>
+            </a>
+          </Link>
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
